17d7dd3793427c57132310b02967aa4b8f52da8d
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2020-01-29 18:32:02 -0400

    ✨ add select word feature

--- src/components/Words.tsx
+++ src/components/Words.tsx
@@ -1,12 +1,14 @@
 import React, { FC } from "react";
 import styled from "@emotion/styled";
-import { Box } from "rebass";
+import { Box, Button } from "rebass";
 
+import { useLocalStorage } from "../hooks/useLocalStorage";
 import { useWords } from "../hooks/useWords";
 
 const WordsContainer = styled(Box)`
   align-self: center;
   margin: 0 auto;
+  text-align: center;
 `;
 
 const WordContainer = styled(Box)`
@@ -16,6 +18,10 @@ const WordContainer = styled(Box)`
   padding: 1rem;
   border-radius: 1rem;
   border: 2px solid black;
+`;
+
+const ClickableWordContainer = styled(WordContainer)`
+  cursor: pointer;
 `;
 
 const Word = styled(Box)`
@@ -28,17 +34,47 @@ const Definition = styled(Box)`
   word-break: break-word;
 `;
 
+const ResetButton = styled(Button)`
+  cursor: pointer;
+  color: black;
+  border: 2px solid black;
+  border-radius: 0.5rem;
+`;
+
+interface CurrentWord {
+  word: string;
+  definition: string;
+}
+
 export const Words: FC = () => {
+  const [currentWord, setCurrentWord] = useLocalStorage<
+    CurrentWord | undefined
+  >("word", undefined);
   const words = useWords({ number: 3 });
 
   return (
     <WordsContainer>
-      {words.map(({ word, definition }) => (
-        <WordContainer key={word}>
-          <Word>{word}</Word>
-          <Definition>{definition}</Definition>
-        </WordContainer>
-      ))}
+      {currentWord ? (
+        <>
+          <WordContainer>
+            <Word>{currentWord.word}</Word>
+            <Definition>{currentWord.definition}</Definition>
+          </WordContainer>
+          <ResetButton onClick={() => setCurrentWord(undefined)}>
+            Reset
+          </ResetButton>
+        </>
+      ) : (
+        words.map(({ word, definition }) => (
+          <ClickableWordContainer
+            key={word}
+            onClick={() => setCurrentWord({ word, definition })}
+          >
+            <Word>{word}</Word>
+            <Definition>{definition}</Definition>
+          </ClickableWordContainer>
+        ))
+      )}
     </WordsContainer>
   );
 };
