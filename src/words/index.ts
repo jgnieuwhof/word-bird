48673759aa187b3d547754cbab686c9739c9fd69
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2020-01-29 14:38:32 -0400

    💯 use advanced word list

--- src/words/index.ts
+++ src/words/index.ts
@@ -1,6 +1,6 @@
 import { parse } from "papaparse";
 
-import theHerald from "./the_herald.csv";
+import theHerald from "./the_herald_advanced.csv";
 
 const sources = [theHerald];
 
@@ -12,5 +12,5 @@ export interface Word {
 export const words: Word[] = sources
   .map(x => parse(x, { header: true, skipEmptyLines: true, trimHeaders: true })) // parse the csv
   .map(x => x.data) // get the data
-  .flatMap(x => x.slice(1)) // remove the header row
+  .flatMap<Word>(x => x.slice(1)) // remove the header row
   .map(x => ({ word: x.word.trim(), definition: x.definition.trim() })); // trim that bud
