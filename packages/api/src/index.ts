62e121559212f7714d010ae05ce86c6a375677eb
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2021-03-23 03:29:06 -0300

    ✨ add a simple express server

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,5 +1,14 @@
-const start = () => {
-  console.log("hey");
-};
+import express from "express";
 
-start();
+import { PORT } from "./config";
+
+const app = express();
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
